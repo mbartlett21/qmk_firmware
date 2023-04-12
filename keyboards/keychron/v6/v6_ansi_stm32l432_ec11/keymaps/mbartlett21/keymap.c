ba5f7c6ad8bd743174da6e9427384364e7358c88
mbartlett21 <mjmouse9999@gmail.com>
2023-04-13 08:56:24 +1000

    Add mouse key layer

--- keyboards/keychron/v6/v6_ansi_stm32l432_ec11/keymaps/mbartlett21/keymap.c
+++ keyboards/keychron/v6/v6_ansi_stm32l432_ec11/keymaps/mbartlett21/keymap.c
@@ -23,10 +23,12 @@ enum layers{
   BASE,
   FN,
   NUM,
+  MOUSE_L,
 };
 
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
+#define KC_MCAP LT(MOUSE_L,KC_CAPS)
 
 const uint16_t PROGMEM caps_word_combo[] = {KC_LSFT, KC_CAPS, COMBO_END};
 
@@ -52,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_PSCR,  KC_PAUS,  RGB_MOD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MSEL,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_LBRC,  KC_RBRC,  KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_SCLN,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_EQL,   KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-        KC_CAPS,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,            KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
+        KC_MCAP,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,            KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,            KC_QUOT,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,               KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         LM(BASE, MOD_LCTL),LM(BASE, MOD_LGUI),LM(BASE, MOD_LALT),   KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT),
     [BASE] = LAYOUT_ansi_109(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_PSCR,  KC_PAUS,  RGB_MOD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MSEL,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
+        KC_MCAP,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT),
     [FN] = LAYOUT_ansi_109(
@@ -76,6 +78,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,                                KC_P4,    KC_P5,    KC_P6,
         KC_P1,              KC_P2,    KC_P3,    KC_PENT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,            XXXXXXX,            KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_P0,    KC_P0,    KC_PDOT,                                KC_PENT,                                XXXXXXX,  XXXXXXX,  XXXXXXX,  TG(NUM),  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_P0,              KC_PDOT),
+    [MOUSE_L] = LAYOUT_ansi_109(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  KC_WH_U,  KC_WH_L,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  KC_BTN1,  KC_BTN3,  KC_BTN2,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,                                _______,  _______,  _______,
+        _______,            _______,  KC_WH_D,  KC_WH_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_MS_U,            _______,  _______,  _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,            _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -84,6 +93,7 @@ const uint16_t PROGMEM encoder_map[][1][2] = {
     [BASE]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [FN]     = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [NUM]    = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [MOUSE_L]= {ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
 };
 #endif // ENCODER_MAP_ENABLE
 
