1c5b6462e4efeac542d63b1adeee1e0166c4f05d
mbartlett21 <mjmouse9999@gmail.com>
2023-12-13 15:49:59 +1000

    Add extra Fn keys

--- keyboards/keychron/v6/v6_ansi_stm32l432_ec11/keymaps/mbartlett21/keymap.c
+++ keyboards/keychron/v6/v6_ansi_stm32l432_ec11/keymaps/mbartlett21/keymap.c
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT),
     [FN] = LAYOUT_ansi_109(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  RGB_TOG,  _______,  _______,  KC_SCRL,  _______,  _______,  _______,  _______,
-        QK_RBT,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_CALC,  _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        QK_RBT,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_CALC,  _______,  _______,  _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MSEL,  _______,  _______,  _______,  _______,
         TG(MOUSE_L),RGB_RMOD,RGB_VAD, RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  DM_PLY1,  DM_PLY2,  _______,            _______,                                ISS_MAN,  PAST_TB,  KEY_MJB,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  DM_REC1,  DM_REC2,  DM_RSTP,            _______,            _______,            ISS_APP,  _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,TG(MOUSE_L),_______,  TO(NUM),  _______,  _______,  _______,  _______,            _______),
