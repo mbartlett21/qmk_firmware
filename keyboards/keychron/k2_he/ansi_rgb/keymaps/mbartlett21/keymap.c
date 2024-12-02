c9efb2212a7e25345b4a2f37ce12969bc1fb7be5
mbartlett21 <mjmouse9999@gmail.com>
2024-12-02 11:42:55 +1000

    Add mouse layer and dynamic macros

--- keyboards/keychron/k2_he/ansi_rgb/keymaps/mbartlett21/keymap.c
+++ keyboards/keychron/k2_he/ansi_rgb/keymaps/mbartlett21/keymap.c
@@ -22,7 +22,10 @@ enum layers {
     MAC_FN,
     WIN_BASE,
     WIN_FN,
+    WIN_MSE,
 };
+
+#define KC_MCAP LT(WIN_MSE,KC_CAPS)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -62,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*   Tab       Q         W         E         R         T         Y         U         I         O         P         [         ]         \                   PgDn    */
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
 /*   Caps      A         S         D         F         G         H         J         K         L         ;         '                   Enter               Home    */
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_MCAP,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
 /*   Shift               Z         X         C         V         B         N         M         ,         .         /                   Shift     Up        End     */
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
 /*   Ctrl      Gui       Alt                                     Space                                   Alt       WIN_FN    Ctrl      Left      Down      Rght    */
@@ -76,11 +79,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*   Tab       Q         W         E         R         T         Y         U         I         O         P         [         ]         \                   PgDn    */
      RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
 /*   Caps      A         S         D         F         G         H         J         K         L         ;         '                   Enter               Home    */
-     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+   TG(WIN_MSE),RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  DM_PLY1,  DM_PLY2,  _______,            _______,            _______,
 /*   Shift               Z         X         C         V         B         N         M         ,         .         /                   Shift     Up        End     */
-     _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
+     _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  DM_REC1,  DM_REC2,  DM_RSTP,            _______,  _______,  _______,
 /*   Ctrl      Gui       Alt                                     Space                                   Alt       WIN_FN    Ctrl      Left      Down      Rght    */
-     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______)
+     _______,  _______,  _______,                                _______,                              TG(WIN_MSE),_______,  _______,  _______,  _______,  _______)
+
+[WIN_MSE] = LAYOUT_ansi_84(
+/*   Esc       F1        F2        F3        F4        F5        F6        F7        F8        F9        F10       F11       F12       PrtSc     Delete    Lights  */
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+/*   `         1         2         3         4         5         6         7         8         9         0         -         =         Bksp                PgUp    */
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+/*   Tab       Q         W         E         R         T         Y         U         I         O         P         [         ]         \                   PgDn    */
+     _______,  _______,  KC_WH_U,  KC_WH_L,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+/*   Caps      A         S         D         F         G         H         J         K         L         ;         '                   Enter               Home    */
+   TG(WIN_MSE),KC_BTN1,  KC_BTN3,  KC_BTN2,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+/*   Shift               Z         X         C         V         B         N         M         ,         .         /                   Shift     Up        End     */
+     _______,            _______,  KC_WH_D,  KC_WH_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  KC_MS_U,  _______,
+/*   Ctrl      Gui       Alt                                     Space                                   Alt       WIN_FN    Ctrl      Left      Down      Rght    */
+     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R)
 };
 
 // clang-format on
